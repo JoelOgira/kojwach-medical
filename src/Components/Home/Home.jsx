ae085806a387fe7348007cb827799fc7bf800d36
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-13 15:54:43 +0300

    Created Home component

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Insurance from "./Insurance";
+import Showcase from "./Showcase";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Showcase />
+            <Insurance />
+        </div>
+    )
+}
+
+export default Home;
