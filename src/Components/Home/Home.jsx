fa557861169ae6be2b7df4588aced8bfeb0f8563
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-14 17:41:13 +0300

    Modified styling

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,15 @@
+import { EmblaCarousel } from "./EmblaCarousel";
 import Insurance from "./Insurance";
 import Showcase from "./Showcase";
+
 
 const Home = () => {
 
     return (
         <div>
+            <div className="carousel w-full h-[300px] sm:h-[400px] md:h-[600px]">
+                <EmblaCarousel />
+            </div>
             <Showcase />
             <Insurance />
         </div>
