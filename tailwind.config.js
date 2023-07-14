d87c9569734f04b61242f194612f1d8901164ac6
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-14 17:40:00 +0300

    Added custom color styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        primary: "#fff0f0",
+        secondary: "#b12828"
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
