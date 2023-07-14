d4a9d196c444e04a9e946dafd00d3b3d45f8db9d
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-14 17:41:39 +0300

    Modified styling

--- src/Navigation/Header.jsx
+++ src/Navigation/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import logo from "../assets/logo.png";
 import { FaPhoneAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [ isOpen, setIsOpen ] = useState(true);
@@ -15,17 +16,17 @@ const Header = () => {
     }
 
     return (
-        <header className="w-full top-0 px-5 py-5 bg-[#fff0f0] md:px-0">
+        <header className="w-full top-0 shadow-xl px-5 py-10 md:px-0">
             <div className="flex items-center space-x-6 justify-between md:container md:w-5/6 md:mx-auto lg:w-3/4">
-                <div className="pt-2">
+                <Link to="/" className="pt-2">
                     <img src={logo} alt="Site Icon" width="100px" />
-                </div>
-                <div className="hidden md:flex space-x-5 font-bold">
-                    <a href="/#" className="hover:text-[#ec4949]">Home</a>
-                    <a href="/#" className="hover:text-[#ec4949]">Departments</a>
-                    <a href="/#" className="hover:text-[#ec4949]">Services</a>
-                    <a href="/#" className="hover:text-[#ec4949]">Services</a>
-                    <a href="/#" className="hover:text-[#ec4949]">Contact</a>
+                </Link>
+                <div className="hidden md:flex space-x-10 font-bold">
+                    <Link to="/" className="hover:text-[#ec4949]">Home</Link>
+                    <Link to="/departments" className="hover:text-[#ec4949]">Departments</Link>
+                    <Link to="/services" className="hover:text-[#ec4949]">Services</Link>
+                    <Link to="/about" className="hover:text-[#ec4949]">About Us</Link>
+                    <Link to="/contact" className="hover:text-[#ec4949]">Contact</Link>
                 </div>
 
                 <button className="hidden p-3 px-6 text-[#ec4949] border-2 border-[#ec4949] rounded-md baseline hover:bg-[#ec4949] hover:text-white md:flex md:space-x-2">
@@ -46,12 +47,12 @@ const Header = () => {
             </div>
 
             <div className="md:hidden">
-                <div id="menu" className={`absolute flex-col ${isHidden ? 'hidden' : 'flex'} items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}>
-                    <a href="/#">Home</a>
-                    <a href="/#">Departments</a>
-                    <a href="/#">Services</a>
-                    <a href="/#">Services</a>
-                    <a href="/#">Contact</a>
+                <div id="menu" className={`flex-col ${isHidden ? 'hidden' : 'flex'} items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}>
+                    <Link to="/">Home</Link>
+                    <Link to="/departments">Departments</Link>
+                    <Link to="/services">Services</Link>
+                    <Link to="/about">About Us</Link>
+                    <Link to="/contact">Contact</Link>
                 </div>
             </div>
         </header>
