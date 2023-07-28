e54a3a722a587482a453ea06a1959129bb8c99fb
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-28 23:42:52 +0300

    Updated styling in the component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -24,10 +24,10 @@ const About = () => {
                 <div className="flex flex-col space-y-10 md:flex-row md:space-y-0 md:space-x-10 mb-5">
                     <div className="flex flex-col space-y-5 md:w-1/2">
                         <h2 className="text-secondary font-bold text-3xl">About Us</h2>
-                        <p>
+                        <p className="md:max-w-lg">
                             Kojwach Medical Center Limited is a leading Public Hospital (Level 3) with a 30-bed hospital equipped with state-of-the-art medical equipment. We employ cutting edge research, innovation, training, and partnerships to keep abreast with advancements in healthcare management. This enables us to address the current health challenges and ensures that we are responsive to emerging and future health challenges.
                         </p>
-                        <p>
+                        <p className="md:max-w-lg">
                             We offer quality patient-centered care where the patient is an active participant in their care. This brings about improved patient health outcomes and satisfaction and improved job satisfaction among our healthcare providers. Specialized care is one of our flagship services where patients are attended to by highly specialized consultants.
                         </p>
                     </div>
@@ -73,29 +73,32 @@ const About = () => {
                         </p>
                     </div>
 
-                    <div className="grid grid-cols-2 gap-10 justify-center items-center md:grid-cols-4 md:gap-5">
-                        <div className="flex flex-col justify-center items-center space-y-8 border-r">
+                    <div className="grid grid-cols-2 justify-center items-center md:grid-cols-4">
+                        <div className="flex flex-col h-full justify-center items-center space-y-4 border border-r-0 border-b-0 py-5 border-secondary hover:bg-secondary hover:text-white md:border-b">
                             <TbMicroscope className="text-6xl" />
-                            <div className="font-semibold text-xl text-center">
-                                <p>State-of-the-art</p>
+                            <div className="font-semibold text-xl text-center max-w-sm">
+                                <p className="px-5 md:px-0">State-of-the-art</p>
                                 <p>Equipment</p>
                             </div>
                         </div>
-                        <div className="flex flex-col justify-center items-center space-y-8 md:border-r">
+
+                        <div className="flex flex-col h-full justify-center items-center space-y-4 border border-b-0 py-5 border-secondary hover:bg-secondary hover:text-white md:border-r-0 md:border-b">
                             <GiGears className="text-6xl" />
                             <div className="font-semibold text-xl text-center">
                                 <p>Seamless</p>
                                 <p>Systems & Processes</p>
                             </div>
                         </div>
-                        <div className="flex flex-col justify-center items-center space-y-8 border-r">
+
+                        <div className="flex flex-col h-full justify-center items-center space-y-4 border border-r-0 py-5 text-center border-secondary hover:bg-secondary hover:text-white">
                             <GiClockwork className="text-6xl" />
                             <div className="font-semibold text-xl text-center">
                                 <p>24/7</p>
                                 <p>Operations</p>
                             </div>
                         </div>
-                        <div className="flex flex-col justify-center items-center space-y-8">
+
+                        <div className="flex flex-col h-full justify-center items-center space-y-4 border py-5 border-secondary hover:bg-secondary hover:text-white">
                             <FaUsersGear className="text-6xl" />
                             <div className="font-semibold text-xl text-center">
                                 <p>Expert</p>
