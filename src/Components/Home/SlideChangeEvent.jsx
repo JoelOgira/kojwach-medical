3bde24a1aba9ba559a30c52a388dc2a82fc2fe78
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-11 21:08:32 +0300

    Added more content and styling

--- src/Components/Home/SlideChangeEvent.jsx
+++ src/Components/Home/SlideChangeEvent.jsx
@@ -1,41 +1,72 @@
 'use client';
 
 import { Carousel } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 export default function SlideChangeEvent() {
   return (
     <Carousel>
       <div className="slider-1 flex h-full w-full items-center justify-center">
-        <div className="bg-black w-full h-full bg-opacity-60">
+        <div className="bg-white w-full h-full bg-opacity-60">
           <div className="px-16 py-8 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4 h-full">
             <div className="flex justify-start items-center text-center h-full md:text-start">
-              <h1 className='text-5xl text-white max-w-md font-semibold'>
-                EXPERIENCE QUALITY & AFFORDABLE HEALTHCARE
-              </h1>
+              <div className="flex flex-col space-y-4">
+                <h1 className='text-3xl text-black max-w-md font-semibold md:text-5xl sm:text-left'>
+                  EXPERIENCE QUALITY & AFFORDABLE HEALTHCARE
+                </h1>
+                <p className="hidden max-w-md font-semibold md:block">
+                  We provide access to comprehensive and integrated quality health care to improve individual and community health
+                </p>
+                <div className="flex justify-center sm:justify-start">
+                  <button className='flex justify-center sm:justify-start bg-tertiary text-white rounded-full py-3 px-6 hover:bg-secondary'>
+                    <Link to='/about'>Know more</Link>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
       <div className="slider-2 flex h-full w-full items-center justify-center">
-        <div className="bg-black w-full h-full bg-opacity-60">
+        <div className="bg-white w-full h-full bg-opacity-60">
           <div className="px-16 py-8 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4 h-full">
             <div className="flex justify-center items-center h-full text-center">
-              <h1 className='text-5xl text-white max-w-md font-semibold'>
-                EXPERIENCE QUALITY & AFFORDABLE HEALTHCARE
-              </h1>
+              <div className="flex flex-col space-y-4">
+                <h1 className='text-3xl text-black max-w-xl font-semibold md:text-5xl'>
+                  EXPERIENCE QUALITY & AFFORDABLE HEALTHCARE
+                </h1>
+                <p className="hidden max-w-md font-semibold md:block">
+                  We provide access to comprehensive and integrated quality health care to improve individual and community health
+                </p>
+                <div className="flex justify-center">
+                  <button className='mx-auto bg-tertiary text-white rounded-full py-3 px-6 hover:bg-secondary'>
+                    <Link to='/about'>Know more</Link>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
       <div className="slider-3 flex h-full w-full items-center justify-center">
-        <div className="bg-black w-full h-full bg-opacity-60">
+        <div className="bg-white w-full h-full bg-opacity-60">
           <div className="px-16 py-8 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4 h-full">
             <div className="flex justify-end items-center text-center h-full md:text-right">
-              <h1 className='text-5xl text-white max-w-md font-semibold'>
-                EXPERIENCE QUALITY & AFFORDABLE HEALTHCARE
-              </h1>
+              <div className="flex flex-col space-y-4">
+                <h1 className='text-3xl text-black max-w-md font-semibold md:text-5xl sm:text-right'>
+                  EXPERIENCE QUALITY & AFFORDABLE HEALTHCARE
+                </h1>
+                <p className="hidden max-w-md font-semibold md:block">
+                  We provide access to comprehensive and integrated quality health care to improve individual and community health
+                </p>
+                <div className="flex justify-center sm:justify-end">
+                  <button className='flex justify-center sm:justify-start bg-tertiary text-white rounded-full py-3 px-6 hover:bg-secondary'>
+                    <Link to='/about'>Know more</Link>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
