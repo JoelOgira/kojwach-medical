550589632a34b895f8341781ebf63fc9d5084589
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-12 22:56:55 +0300

    Created community component

---
+++ src/Components/Home/Community.jsx
@@ -0,0 +1,52 @@
+
+
+const Community = () => {
+    return (
+        <section className="community-bg w-full">
+            <div className="px-5 py-4 md:px-0 md:container md:mx-auto md:py-18 md:pt-24 md:w-5/6 lg:w-3/4">
+                <div className="flex flex-col space-y-10 md:flex-row lg:space-y-0 lg:space-x-10">
+
+                    <div className="flex flex-col space-y-5 md:w-2/3">
+                        <h2 className="text-secondary font-bold text-xl">To the Community</h2>
+                        <h2 className="font-bold text-2xl md:text-5xl md:max-w-4xl">Focusing On General Community Development & Health</h2>
+                        <p className=" font-semibold text-lg md:max-w-3xl">
+                            Matata Nursing Hospital provides access to comprehensive and integrated quality health care to improve individual and community health in Homa Bay and other Nyanza Counties.
+                        </p>
+                        <p className="md:max-w-3xl">
+                            Additionally, Matata Nursing Hospital has programs focused on community development through the MNH’s Community Social Responsibility such as renovation of schools, safe clean supply development and sustainable conservation agriculture. These activities are provided to mainly improve the wellness and wellbeing of the communities it serves.
+                        </p>
+                    </div>
+
+                    <div className="w-full flex flex-col space-y-5 md:justify-end md:items-baseline md:w-1/3">
+                        <div className="w-full px-6 py-7 flex justify-start items-center space-x-7">
+                            <div className="staff-stat p-8 text-2xl font-semibold">20+</div>
+                            <div className="flex flex-col space-y-2">
+                                <p className="font-semibold text-xl">Qualified Personnel</p>
+                                <p>Trained experts present in our clinic.</p>
+                            </div>
+                        </div>
+
+                        <div className="w-full rounded-md bg-white px-6 py-7 flex justify-start items-center space-x-7">
+                            <div className="services-stat p-8 text-2xl font-semibold">10+</div>
+                            <div className="flex flex-col space-y-2">
+                                <p className="font-semibold text-xl">Medical Services</p>
+                                <p>Total medical services provided by KMC</p>
+                            </div>
+                        </div>
+
+                        <div className="w-full px-6 py-7 flex justify-start items-center space-x-7">
+                            <div className="bed-stat p-10 text-2xl font-semibold">40</div>
+                            <div className="flex flex-col space-y-2">
+                                <p className="font-semibold text-xl">Bed Capacity</p>
+                                <p>Total bed capacity in our hospital</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Community;
