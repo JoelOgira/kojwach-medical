e2aca1200b219d24aa072307c1b2a10b5011e3a5
Jay <joelogira@gmail.com>
2023-12-02 21:57:52 +0300

    Changed styling in the component

--- src/Components/Home/Community.jsx
+++ src/Components/Home/Community.jsx
@@ -5,9 +5,9 @@ const Community = () => {
             <div className="px-5 py-4 md:px-0 md:container md:mx-auto md:py-18 md:pt-24 md:w-5/6 lg:w-3/4">
                 <div className="flex flex-col space-y-10 md:flex-row lg:space-y-0 lg:space-x-10">
                     <div className="flex flex-col space-y-5 md:w-2/3">
-                        <h3 className="text-secondary font-bold text-xl">To the Community</h3>
-                        <h2 className="font-bold text-2xl md:text-5xl md:max-w-4xl">Focusing On General Community Development & Health</h2>
-                        <p className=" font-semibold text-lg md:max-w-3xl">
+                        <h3 className="text-secondary font-bold font-poppins text-xl">To the Community</h3>
+                        <h2 className="font-bold font-poppins text-2xl md:text-5xl md:max-w-4xl">Focusing On General Community Development & Health</h2>
+                        <p className=" font-semibold font-poppins text-lg md:max-w-3xl">
                             Kojwach Medical Center provides access to comprehensive and integrated quality health care to improve individual and community health in Homa Bay and other Nyanza Counties.
                         </p>
                         <p className="md:max-w-3xl">
@@ -17,25 +17,25 @@ const Community = () => {
 
                     <div className="w-full flex flex-col space-y-5 md:justify-end md:items-baseline md:w-1/3">
                         <div className="w-full px-6 py-7 flex justify-start items-center space-x-7">
-                            <div className="staff-stat p-8 text-2xl font-semibold">20+</div>
+                            <div className="staff-stat p-8 text-2xl font-semibold font-poppins">20+</div>
                             <div className="flex flex-col space-y-2">
-                                <p className="font-semibold text-xl">Qualified Personnel</p>
+                                <p className="font-semibold font-poppins text-xl">Qualified Personnel</p>
                                 <p>Trained experts present in our clinic.</p>
                             </div>
                         </div>
 
                         <div className="w-full rounded-md bg-white px-6 py-7 flex justify-start items-center space-x-7">
-                            <div className="services-stat p-8 text-2xl font-semibold">10+</div>
+                            <div className="services-stat p-8 text-2xl font-semibold font-poppins">10+</div>
                             <div className="flex flex-col space-y-2">
-                                <p className="font-semibold text-xl">Medical Services</p>
+                                <p className="font-semibold font-poppins text-xl">Medical Services</p>
                                 <p>Total medical services provided by KMC</p>
                             </div>
                         </div>
 
                         <div className="w-full px-6 py-7 flex justify-start items-center space-x-7">
-                            <div className="bed-stat p-10 text-2xl font-semibold">40</div>
+                            <div className="bed-stat p-10 text-2xl font-semibold font-poppins">40</div>
                             <div className="flex flex-col space-y-2">
-                                <p className="font-semibold text-xl">Bed Capacity</p>
+                                <p className="font-semibold font-poppins text-xl">Bed Capacity</p>
                                 <p>Total bed capacity in our hospital</p>
                             </div>
                         </div>
