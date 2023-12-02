b497f0a1551adbbd3753f35b58cb6c4ecb239e9b
Jay <joelogira@gmail.com>
2023-12-02 21:56:19 +0300

    Changed font in the component

--- src/Components/Home/Showcase.jsx
+++ src/Components/Home/Showcase.jsx
@@ -7,7 +7,7 @@ const Showcase = () => {
         <section className="w-full showcase">
             <div className="px-5 py-10 mb-10 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4">
                 <div className="flex flex-col space-y-5 py-20 justify-center items-center text-center w-full">
-                    <h2 className="font-bold text-3xl max-w-md md:text-4xl md:max-w-xl">Welcome to our facility that provides quality care for all</h2>
+                    <h2 className="font-bold font-poppins text-3xl max-w-md md:text-4xl md:max-w-xl">Welcome to our facility that provides quality care for all</h2>
                     <p className="text-lg">If you have any questions about testing, insurance coverage, duration of services.</p>
                 </div>
 
@@ -18,7 +18,7 @@ const Showcase = () => {
                                 <div className="p-6 rounded-full bg-[#ec4949]">
                                     <TbMicroscope className="text-white text-6xl" />
                                 </div>
-                                <h3 className="text-3xl max-w-xs font-semibold">Specialized Care</h3>
+                                <h3 className="text-3xl max-w-xs font-semibold font-poppins">Specialized Care</h3>
                                 <p className="text-center">We have comprehensive specialized clinics for adults and children where patients are attended to by highly specialized consultants.</p>
                             </div>
                         </div>
@@ -28,7 +28,7 @@ const Showcase = () => {
                                 <div className="p-6 rounded-full bg-white">
                                     <GiHospital className="text-[#ec4949] text-6xl" />
                                 </div>
-                                <h3 className="text-3xl max-w-xs font-semibold">Our Hospital</h3>
+                                <h3 className="text-3xl max-w-xs font-semibold font-poppins">Our Hospital</h3>
                                 <p className="text-center">We offer quality patient-centered care where the patient is an active participant in their care. This brings about improved patient health outcomes and satisfaction.</p>
                             </div>
                         </div>
@@ -37,9 +37,9 @@ const Showcase = () => {
                     <div className="md:w-1/3">
                         <div className="flex flex-col space-y-6 bg-[#b12828] rounded-md justify-center items-center text-center py-10  px-8 md:px-6 text-white">
                             <div className="p-6 rounded-full bg-[#ec4949]">
-                                <BsShieldCheck className="text-white font-bold text-6xl" />
+                                <BsShieldCheck className="text-white font-bold font-poppins text-6xl" />
                             </div>
-                            <h3 className="text-3xl max-w-xs font-semibold">Most Insurances Accepted</h3>
+                            <h3 className="text-3xl max-w-xs font-semibold font-poppins">Most Insurances Accepted</h3>
                             <p className="text-center">We accept a number of insurance policies from different vendors. This gives you a piece of mind while visiting our facility for top-notch healthcare.</p>
                         </div>
                     </div>
