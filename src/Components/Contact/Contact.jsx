718511b80f88002cd5fd65032ad75653f4662277
Jay <joelogira@gmail.com>
2023-12-03 22:07:33 +0300

    Changed styling in the component

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
                     <div className="px-5 py-8 md:px-0 md:container md:mx-auto md:w-5/6 lg:w-3/4">
                         <div className="flex space-x-5 mb-3">
                             <div className="bg-[#b12828] h-10 w-2"></div>
-                            <h1 className="text-[#b12828] font-bold text-4xl">Contact Us</h1>
+                            <h1 className="text-[#b12828] font-bold font-poppins text-4xl">Contact Us</h1>
                         </div>
                         <p className="text-secondary"><Link to="/" className="underline">Home</Link> - Contact Us</p>
                     </div>
@@ -29,12 +29,12 @@ const Contact = () => {
                 <div className="flex flex-col space-y-5">
                     <div className="flex flex-col w-full md:flex-row md:space-y-0 md:space-x-20">
                         <div className="w-full grid content-center text-start justify-start px-5 md:px-0 md:w-1/3 md:justify-start md:items-start md:text-left">
-                            <h2 className="pb-8 text-tertiary text-2xl pt-8 font-semibold underline">Contact Us</h2>
+                            <h2 className="pb-8 text-tertiary text-2xl pt-8 font-semibold font-poppins underline">Contact Us</h2>
 
                             <div className="w-full flex flex-row space-x-5 border-b pb-8 text-left">
                                 <VscLocation className="text-4xl text-tertiary" />
                                 <div>
-                                    <h4 className="font-semibold pb-3 text-2xl text-tertiary">Address</h4>
+                                    <h4 className="font-semibold font-poppins pb-3 text-2xl text-tertiary">Address</h4>
                                     <p className="max-w-sm text-sm">P.O Box 384-4020 Homabay</p>
                                     <p className="max-w-sm text-sm">Off Kisii-Kisumu Highway, Ringa Kojwach</p>
                                 </div>
@@ -43,7 +43,7 @@ const Contact = () => {
                             <div className="w-full flex flex-row space-x-5 border-b py-8 text-left">
                                 <LiaPhoneVolumeSolid className="text-3xl mt-1 text-tertiary" />
                                 <div>
-                                    <h4 className="font-semibold pb-3 text-2xl text-tertiary">Call Us</h4>
+                                    <h4 className="font-semibold font-poppins pb-3 text-2xl text-tertiary">Call Us</h4>
                                     <p className="max-w-sm text-sm">Tel: +2547987654</p>
                                     <p className="max-w-sm text-sm">Alt: +2547123456</p>
                                     <p className="max-w-sm text-sm">WhatsApp: +2547987654</p>
@@ -53,7 +53,7 @@ const Contact = () => {
                             <div className="w-full flex flex-row space-x-5 border-b py-8 text-left">
                                 <TfiEmail className="text-3xl text-tertiary" />
                                 <div>
-                                    <h4 className="font-semibold pb-3 text-2xl text-tertiary">Email Address</h4>
+                                    <h4 className="font-semibold font-poppins pb-3 text-2xl text-tertiary">Email Address</h4>
                                     <p className="max-w-sm text-sm">info@kojwachmedical.com</p>
                                 </div>
                             </div>
@@ -61,7 +61,7 @@ const Contact = () => {
                             <div className="w-full flex flex-row space-x-5 border-b py-8 text-left">
                                 <IoMdClock className="text-4xl text-tertiary" />
                                 <div>
-                                    <h4 className="font-semibold pb-3 text-2xl text-tertiary">Visiting Hours</h4>
+                                    <h4 className="font-semibold font-poppins pb-3 text-2xl text-tertiary">Visiting Hours</h4>
                                     <p className="max-w-sm text-sm">7:00 am - 9:00 am</p>
                                     <p className="max-w-sm text-sm">1:00 pm - 2:00 pm</p>
                                     <p className="max-w-sm text-sm">4:00 pm - 6:00 pm</p>
@@ -88,7 +88,7 @@ const Contact = () => {
                         <div className="w-full shadow-md md:w-2/3 md:my-0">
                             <div className="px-5">
                                 <h2 className="text-tertiary pt-6 pb-3">GET IN TOUCH</h2>
-                                <p className="text-tertiary font-bold text-3xl">Visit Us</p>
+                                <p className="text-tertiary font-bold font-poppins text-3xl">Visit Us</p>
                             </div>
                             <img src={map} alt="Google maps" className="mt-4 h-full" />
                         </div>
